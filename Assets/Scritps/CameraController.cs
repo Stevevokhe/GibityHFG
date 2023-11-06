8304741d2f4440dc442e19a747874f34a0c53da2
Linux1230 <b.richi1230@gmail.com>
2023-11-06 22:10:39 +0100

    Basic Camera movement (needs some work) (#2)

--- Assets/Scritps/CameraController.cs
+++ Assets/Scritps/CameraController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
@@ -8,10 +6,18 @@ public class CameraController : MonoBehaviour
     private Transform player;
     [SerializeField]
     private Vector3 offset;
+    [SerializeField]
+    private float smoothTime = 0.5f;
+
+    private Vector3 velocity = Vector3.zero;
 
     private void Update()
     {
-        // Camera follows the player with specified offset position
-        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z);
+        if (player == null)
+        {
+            return;
+        }
+        var targetPosition = new Vector3(player.position.x + offset.x, player.position.y + offset.y, transform.position.z + offset.z);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
 }
