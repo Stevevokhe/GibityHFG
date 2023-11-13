ff5054d043a71fc9089580f213a1881ee3beb1f8
Linux1230 <b.richi1230@gmail.com>
2023-11-13 20:41:33 +0100

    add newtonsoftJson, add SavingManager

---
+++ Assets/Scritps/System/SavingData/IPlatformSavingManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public enum SavingManagerResponseStatus { Success, LocalError, NetworkError, LocalandNetworkError, HasntSave }
+
+public interface IPlatformSavingManager
+{
+    void Save();
+
+    void DeleteSavedData();
+
+    #region Volume
+
+    void SetMusicVolume(float value);
+
+    float GetMusicVolume(float defaultValue);
+
+    void SetSFXVolume(float value);
+
+    float GetSFXVolume(float defaultValue);
+    #endregion
+
+    #region GameProgress
+    void SetGameProgress(GameProgress gameProgress, Action<SavingManagerResponseStatus> callback);
+
+    void GetGameProgress(Action<GameProgress, SavingManagerResponseStatus> callback);
+    #endregion
+}
---
+++ Assets/Scritps/System/SavingData/SavingManagerType.cs
@@ -0,0 +1,7 @@
+﻿public static class SavingManagerType
+{
+    public const string MasterVolume = "MasterVolume";
+    public const string MusicVolume = "MusicVolume";
+    public const string SFXVolume = "SFXVolume";
+    public const string GameProgress = "GameProgress";
+}
