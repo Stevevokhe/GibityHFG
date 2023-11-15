cf7825dc439b84f31dca26a4368183f246643bdb
Xardas0327 <32038221+Xardas0327@users.noreply.github.com>
2023-11-15 19:35:49 +0000

    Add sounds to game

--- Assets/Scritps/Player/PlayerController.cs
+++ Assets/Scritps/Player/PlayerController.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(AudioSource))]
 [RequireComponent(typeof(PlayerAgeController))]
 public class PlayerController : MonoBehaviour
 {
@@ -20,11 +22,20 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private LayerMask enemyLayer;
 
+    [Header("Sounds")]
+    [SerializeField]
+    private AudioClip jumpSound;
+    [SerializeField]
+    private AudioClip[] steepSounds;
+
     private Rigidbody2D rb;
     private bool isGrounded;
     private int points = 0;
     private PlayerAgeController playerAgeController;
+    private AudioSource audioSource;
     private HashSet<Key> keys = new();
+    private bool isWaitingForNextStepSound = false;
+    private int stepSoundIndex = 0;
 
     public event EventHandler<int> ChangedPoints;
     public event EventHandler<Key> AddedNewKey;
@@ -45,11 +56,18 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
         playerAgeController = GetComponent<PlayerAgeController>();
         playerAgeController.AgeTimerEnded += (s, e) => Debug.Log("Age Timer Ended");
         playerAgeController.AgeTimerStarted += (s, e) => Debug.Log("Age Timer Started");
         playerAgeController.MaxAgeLimitReached += (s, e) => Debug.Log("Max Age Reached");
         playerAgeController.RaiseModelChangeAtAge += (s, e) => Debug.Log($"Model Changed at: {e}");
+
+        if (jumpSound == null)
+            throw new Exception($"{name}: the {nameof(jumpSound)} can't be null.");
+
+        if (steepSounds == null || steepSounds.Length == 0)
+            throw new Exception($"{name}: the {nameof(steepSounds)} can't be empty.");
     }
 
     private void Start()
@@ -59,14 +77,9 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        // Check for grounded state
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+        CheckGrounded();
 
-        // Jump input
-        if (isGrounded && Input.GetButtonDown("Jump"))
-        {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        }
+        CheckJumpInput();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -80,11 +93,50 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        CheckMoveInput();
+    }
+
+    private void CheckGrounded()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+    }
+
+    private void CheckJumpInput()
+    {
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            audioSource.clip = jumpSound;
+            audioSource.Play();
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void CheckMoveInput()
+    {
         // Horizontal movement input
         float horizontalInput = Input.GetAxis("Horizontal");
         Vector2 moveDirection = new Vector2(horizontalInput, 0);
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, rb.velocity.y);
+
+        if(isGrounded && !isWaitingForNextStepSound)
+        {
+            audioSource.clip = steepSounds[stepSoundIndex];
+            audioSource.Play();
+            StartCoroutine(WaitingForNextStepSound(audioSource.clip.length));
+
+            ++stepSoundIndex;
+            if (stepSoundIndex >= steepSounds.Length)
+                stepSoundIndex = 0;
+        }
     }
+
+    private IEnumerator WaitingForNextStepSound(float waitingTime)
+    {
+        isWaitingForNextStepSound = true;
+        yield return new WaitForSeconds(waitingTime);
+        isWaitingForNextStepSound = false;
+    }
+
 
     public void AddKey(Key key)
     {
