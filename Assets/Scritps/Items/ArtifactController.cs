90a90a7be477cf4f260c4d9065fa21e3bb6b433a
Xardas0327 <32038221+Xardas0327@users.noreply.github.com>
2023-11-16 17:20:14 +0000

    Add sounds to game (#12)
    * Add sounds to game
    
    * rename folder
    
    * Fix step sound, when you stand

--- Assets/Scritps/Items/ArtifactController.cs
+++ Assets/Scritps/Items/ArtifactController.cs
@@ -1,13 +1,36 @@
+using System.Collections;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(AudioSource))]
 public class ArtifactController : Item
 {
     [SerializeField]
     private int point = 1;
 
+    private AudioSource audioSource;
+    private SpriteRenderer spriteRenderer;
+    private Collider2D artifactCollider;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        artifactCollider = GetComponent<Collider2D>();
+    }
+
     protected override void TouchPlayer(PlayerController player)
     {
         player.Points += point;
+        spriteRenderer.enabled = false;
+        artifactCollider.enabled = false;
+        audioSource.Play();
+        StartCoroutine(WaitForDestroy(audioSource.clip.length));
+    }
+
+    private IEnumerator WaitForDestroy(float waitingTimeForDestroy)
+    {
+        yield return new WaitForSeconds(waitingTimeForDestroy);
         GameObject.Destroy(gameObject);
     }
 }
