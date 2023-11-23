60a247b5b39480f1c4ff515b7c1c8b36a9b0d9a8
Xardas0327 <32038221+Xardas0327@users.noreply.github.com>
2023-11-23 18:24:25 +0000

    add camera boundary

--- Assets/Scritps/System/CameraController.cs
+++ Assets/Scritps/System/CameraController.cs
@@ -8,6 +8,13 @@ public class CameraController : MonoBehaviour
     private Vector3 offset;
     [SerializeField]
     private float smoothTime = 0.5f;
+    [Header("Boundary")]
+    [SerializeField]
+    private bool useBoundary = false;
+    [SerializeField]
+    private Vector2 minBoundary = Vector2.zero;
+    [SerializeField]
+    private Vector2 maxBoundary = Vector2.zero;
 
     private Vector3 velocity = Vector3.zero;
 
@@ -18,6 +25,20 @@ public class CameraController : MonoBehaviour
             return;
         }
         var targetPosition = new Vector3(player.position.x + offset.x, player.position.y + offset.y, transform.position.z + offset.z);
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime * Time.fixedDeltaTime);
+        
+        var newPosition = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime * Time.fixedDeltaTime);
+        if(useBoundary)
+        {
+            if(newPosition.x < minBoundary.x)
+                newPosition.x = minBoundary.x;
+            else if(newPosition.x > maxBoundary.x)
+                newPosition.x = maxBoundary.x;
+
+            if (newPosition.y < minBoundary.y)
+                newPosition.y = minBoundary.y;
+            else if (newPosition.y > maxBoundary.y)
+                newPosition.y = maxBoundary.y;
+        }
+        transform.position = newPosition;
     }
 }
