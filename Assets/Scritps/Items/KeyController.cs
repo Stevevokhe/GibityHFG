e44d7b33d04a502b2555df97f537575200f5bf6d
Linux1230 <b.richi1230@gmail.com>
2023-11-28 23:04:06 +0100

    Revert "open door sprite used (#45)"
    This reverts commit c51b72445a97c28cf5de79cc3d9e56cbbbc22699.

--- Assets/Scritps/Items/KeyController.cs
+++ Assets/Scritps/Items/KeyController.cs
@@ -1,38 +1,19 @@
-using System.Collections;
 using UnityEngine;
-using static Unity.VisualScripting.Member;
 
-[RequireComponent(typeof(AudioSource))]
 public class KeyController : Item
 {
     [SerializeField]
     private Key key;
 
-    private AudioSource audioSource;
-
     private void Awake()
     {
         if (key == null)
             throw new System.Exception(name + ": key is missing.");
-
-        audioSource = GetComponent<AudioSource>();
-        audioSource.volume *= SavingManager.Instance.GetMasterVolume(1) * SavingManager.Instance.GetSFXVolume(1);
     }
 
     protected override void TouchPlayer(PlayerController player)
     {
         player.AddKey(key);
-        audioSource.Play();
-        StartCoroutine(WaitForSound());
-    }
-
-    IEnumerator WaitForSound()
-    {
-        while (audioSource.isPlaying)
-        {
-            yield return null;
-        }
-
         GameObject.Destroy(gameObject);
     }
 }
--- Assets/Scritps/System/DoorController.cs
+++ Assets/Scritps/System/DoorController.cs
@@ -1,31 +1,16 @@
-using System;
 using UnityEngine;
 
-[RequireComponent(typeof(AudioSource))]
 [RequireComponent(typeof(Collider2D))]
 [RequireComponent(typeof(SpriteRenderer))]
 public class DoorController : MonoBehaviour
 {
     [SerializeField]
     private Key key;
-    [SerializeField]
-    private Sprite openSprite;
-
-    private SpriteRenderer spriteRenderer;
-    private Collider2D doorCollider2D;
-    private AudioSource audioSource;
 
     private void Awake()
     {
         if (key == null)
-            throw new Exception(name + ": key is missing.");
-        if (openSprite == null)
-            throw new Exception(name + ": openSprite is missing.");
-
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        doorCollider2D = GetComponent<Collider2D>();
-        audioSource = GetComponent<AudioSource>();
-        audioSource.volume *= SavingManager.Instance.GetMasterVolume(1) * SavingManager.Instance.GetSFXVolume(1);
+            throw new System.Exception(name + ": key is missing.");
     }
 
     protected virtual void OnCollisionEnter2D(Collision2D collision)
@@ -35,9 +20,7 @@ public class DoorController : MonoBehaviour
         {
             if(player.HasKey(key))
             {
-                spriteRenderer.sprite = openSprite;
-                doorCollider2D.enabled = false;
-                audioSource.Play();
+                GameObject.Destroy(gameObject);
             }
         }
     }
