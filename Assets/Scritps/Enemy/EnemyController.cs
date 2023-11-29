a2818ae0ac158f71d3fdebf2578ac0bda496bdf2
Xardas0327 <32038221+Xardas0327@users.noreply.github.com>
2023-11-29 20:55:15 +0000

    Guard (#59)
    * add Guard animation by code
    
    * Add more guards
    
    * Plus one Guard
    
    ---------
    
    Co-authored-by: Stevevokhe <63345005+Stevevokhe@users.noreply.github.com>

--- Assets/Scritps/Enemy/EnemyController.cs
+++ Assets/Scritps/Enemy/EnemyController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class EnemyController : MonoBehaviour
 {
@@ -12,13 +13,17 @@ public class EnemyController : MonoBehaviour
     [SerializeField]
     private Vector2 closeToTarget = new(0.1f, 2f);
     [SerializeField]
-    private int prisonTime = 5; 
+    private int prisonTime = 5;
+    [SerializeField]
+    private Animator animator;
 
     private int targetIndex;
     private bool canMove;
     private int modifier;
 
     public int PrisonTime => prisonTime;
+
+    private const string SpeedId = "Speed";
 
     private void Awake()
     {
@@ -27,10 +32,52 @@ public class EnemyController : MonoBehaviour
             throw new Exception("Points list must have at least 2 elements");
         }
 
+        if (animator == null)
+            throw new Exception($"{name}: the {nameof(animator)} can't be null.");
+
         transform.position = points[0].Transform.position;
         targetIndex = 1;
         canMove = true;
         modifier = 1;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!canMove)
+        {
+            return;
+        }
+
+        var targetVector = points[targetIndex].Transform.position;
+        targetVector.y = transform.position.y;
+        transform.position = Vector3.MoveTowards(transform.position, targetVector, speed * Time.fixedDeltaTime);
+        animator.SetFloat(SpeedId, 1f);
+
+        var targetPosition = points[targetIndex].Transform.position;
+        if (Mathf.Abs(transform.position.x - targetPosition.x) < closeToTarget.x
+            && Mathf.Abs(transform.position.y - targetPosition.y) < closeToTarget.y)
+        {
+            animator.SetFloat(SpeedId, 0f);
+            StartCoroutine(WaitForTime(points[targetIndex].StopTime));
+        }
+    }
+
+    private void LateUpdate()
+    {
+        FixAnimation();
+    }
+
+    private void FixAnimation()
+    {
+        var targetVector = points[targetIndex].Transform.position;
+        if (
+            //The enemy watch left side, but it move to right side.
+            (transform.localScale.x < 0 && transform.position.x < targetVector.x)
+            //The enemy watch right side, but it move to left side.
+            || (transform.localScale.x > 0 && transform.position.x > targetVector.x))
+        {
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        }
     }
 
     private IEnumerator WaitForTime(float stopTime)
@@ -51,24 +98,5 @@ public class EnemyController : MonoBehaviour
         }
 
         targetIndex += modifier;
-    }
-
-    private void Update()
-    {
-        if (!canMove)
-        {
-            return;
-        }
-
-        var targetVector = points[targetIndex].Transform.position;
-        targetVector.y = transform.position.y;
-        transform.position = Vector3.MoveTowards(transform.position, targetVector, speed * Time.deltaTime);
-
-        var targetPosition = points[targetIndex].Transform.position;
-        if (Mathf.Abs(transform.position.x - targetPosition.x) < closeToTarget.x
-            && Mathf.Abs(transform.position.y - targetPosition.y) < closeToTarget.y)
-        {
-            StartCoroutine(WaitForTime(points[targetIndex].StopTime));
-        }
     }
 }
